5f98294bd3c8ffdca60512eb6c16752112139a9b
allisonvp <a.velardep@pucp.edu.pe>
2020-04-27 16:51:51 -0500

    controller,entity y repository de employee

---
+++ src/main/java/com/example/grupo/Repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.grupo.Repository;
+
+import com.example.grupo.Entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,String> {
+}
