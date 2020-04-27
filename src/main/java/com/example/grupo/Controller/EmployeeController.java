b9ce55d5f98f17402caa9bf0dbad82d0a65aa5ed
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-04-27 17:24:37 -0500

    crear employee y guardar

--- src/main/java/com/example/grupo/Controller/EmployeeController.java
+++ src/main/java/com/example/grupo/Controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,6 +22,21 @@ public class EmployeeController {
         model.addAttribute("lista", listaEmp);
         return "employee/listar";
     }
+
+    @GetMapping("/crear")
+    public String crearEmployee(){
+
+
+        return "employee/crear";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarEmployee(EmployeeEntity employee){
+        employeeRepository.save(employee);
+        return "redirect:/employee/lista";
+    }
+
+
 
 
 
